d0c47c133de629322e8e87b7414720d6025184fb
Joshua Venter <spathi@jeminabox.com>
2026-04-19 15:30:23 +1000

    updating to fork for testing

--- ct/atvloadly.sh
+++ ct/atvloadly.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/YKikAMooCow/ProxmoxVED/add/atvloadly/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: <your-github-handle>
+# Author: spathix
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/bitxeno/atvloadly
 
